bb79da3dfd2e0607fecec1a5bb4304d5cbd0f333
Stephen Harrison <Stephen.Harrison@AnalysisUK.com>
2017-01-24 00:50:21 +0000

    Changed component sensor build for the pin version.

--- Build.bat
+++ Build.bat
@@ -4,7 +4,8 @@
 del *.stl
 
 @echo Building STLs
-"C:\Program Files\OpenSCAD\openscad.com" -o ComponentUsageSensor.stl ComponentUsageSensor.scad
+"C:\Program Files\OpenSCAD\openscad.com" -o ComponentUsageSensor.stl -D "usePinMount=false" ComponentUsageSensor.scad
+"C:\Program Files\OpenSCAD\openscad.com" -o ComponentUsageSensor-Pin.stl -D "usePinMount=true" ComponentUsageSensor.scad
 "C:\Program Files\OpenSCAD\openscad.com" -o ReelHolderArm.stl ReelHolderArm.scad
 "C:\Program Files\OpenSCAD\openscad.com" -o Dispenser.stl Dispenser.scad
 "C:\Program Files\OpenSCAD\openscad.com" -o ReelHolder.stl ReelHolder.scad
