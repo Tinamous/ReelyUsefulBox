dd0d8de1c14d8727e21cd2e0f4429b12f716677f
Stephen Harrison <Stephen.Harrison@AnalysisUK.com>
2017-01-30 01:09:59 +0000

    Added builds for multi-block width units.

--- Build.bat
+++ Build.bat
@@ -3,11 +3,26 @@
 @echo Deleting old STL files.
 del *.stl
 
-@echo Building STLs
+@echo Building Component Usage Sensor with screw STLs
 "C:\Program Files\OpenSCAD\openscad.com" -o ComponentUsageSensor.stl -D "usePinMount=false" ComponentUsageSensor.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ComponentUsageSensor-Pin.stl -D "usePinMount=true" ComponentUsageSensor.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ReelHolderArm.stl ReelHolderArm.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o Dispenser.stl Dispenser.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ReelHolder.stl ReelHolder.scad
+
+@echo Building Component Usage Sensor with Pin STLs
+"C:\Program Files\OpenSCAD\openscad.com" -o ComponentUsageSensor-Pin.stl -D "usePinMount=true;numberOfBlocksWide=1" ComponentUsageSensor.scad
+# For LED tape
+"C:\Program Files\OpenSCAD\openscad.com" -o ComponentUsageSensor-Pin-LED.stl -D "usePinMount=true;numberOfBlocksWide=2;tapeWidth=13" ComponentUsageSensor.scad
+# Covers 4 individual 8mm tape (resistor/caps) as opposed to a double wide tape.
+"C:\Program Files\OpenSCAD\openscad.com" -o ComponentUsageSensor-Pin-4Wide-MultiTape.stl -D "usePinMount=true;numberOfBlocksWide=4;repeatTape=true" ComponentUsageSensor.scad
+
+@echo Building Reel Holder Arm STLs
+"C:\Program Files\OpenSCAD\openscad.com" -o ReelHolderArm.stl -D  "numberOfBlocksWide=1" ReelHolderArm.scad
+"C:\Program Files\OpenSCAD\openscad.com" -o ReelHolderArm-2Wide.stl -D  "numberOfBlocksWide=2" ReelHolderArm.scad
+
+@echo Building Dispenser STLs
+"C:\Program Files\OpenSCAD\openscad.com" -o Dispenser.stl -D  "numberOfBlocksWide=1"  Dispenser.scad
+"C:\Program Files\OpenSCAD\openscad.com" -o Dispenser-2Wide.stl -D  "numberOfBlocksWide=2;smdTapeGap=3" Dispenser.scad
+
+@echo Building Reel Holder STLs
+"C:\Program Files\OpenSCAD\openscad.com" -o ReelHolder.stl -D  "numberOfBlocksWide=1"  ReelHolder.scad
+"C:\Program Files\OpenSCAD\openscad.com" -o ReelHolder-2Wide.stl -D  "numberOfBlocksWide=2" ReelHolder.scad
 
 # MountingPlate needs svg and editing so ignore
